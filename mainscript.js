ae70ce7094cc98a3355e5c017c20cfd2a87dd67a
Alex <admin@opsgames.co.uk>
2016-11-16 00:07:36 +0000

    0.4.1
    Performance improvements

--- mainscript.js
+++
@@ -1,8 +0,0 @@
-function fixIt(){
-	try{
-		$(".bModal").remove();
-		$(".popup").remove();
-		document.getElementById("visas_style_div").style.visibility = "hidden";
-		document.getElementsByClassName("bModal")[0].style.visibility = "hidden";
-	}catch(err){}
-}
